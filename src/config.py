b9c8096565b8231b8e14f8327128855138870d8d
Yoni Lerner <yonilern@gmail.com>
2023-04-26 20:18:11 -0700

    Added setting to validator

--- src/config.py
+++ src/config.py
@@ -44,25 +44,25 @@ settings = Dynaconf(
             messages={"combined": "You must authenticate DRBOT. Run first_time_setup.py"}
         ),
         Validator('subreddit',
-                  ne="", is_type_of=str, messages={"operations": "You must set '{name}' in config/settings.toml"}),
+                  ne="", is_type_of=str, messages={"operations": "You must set '{name}' in data/settings.toml"}),
         Validator('log_file', 'praw_log_file', 'wiki_page', 'local_backup_file',
-                  is_type_of=str, messages={"operations": "Invalid '{name}' in config/settings.toml"}),
+                  is_type_of=str, messages={"operations": "Invalid '{name}' in data/settings.toml"}),
         Validator('point_threshold',
-                  gt=0, is_type_of=int, messages={"operations": "{name} ({value}) must be at least 1 in config/settings.toml"}),
+                  gt=0, is_type_of=int, messages={"operations": "{name} ({value}) must be at least 1 in data/settings.toml"}),
         Validator('point_config',
-                  is_type_of=list, condition=validate_points_config, messages={"condition": "Invalid {name} in config/settings.toml"}),
+                  is_type_of=list, condition=validate_points_config, messages={"condition": "Invalid {name} in data/settings.toml"}),
         Validator('expiration_months', 'modmail_truncate_len',
-                  gte=0, is_type_of=int, messages={"operations": "{name} ({value}) must be a whole number (or 0 to turn it off) in config/settings.toml"}),
+                  gte=0, is_type_of=int, messages={"operations": "{name} ({value}) must be a whole number (or 0 to turn it off) in data/settings.toml"}),
         Validator('autoban_mode',
-                  is_in=[1, 2, 3], messages={"operations": """{name} ({value}) in config/settings.toml must be one of the following:
+                  is_in=[1, 2, 3], messages={"operations": """{name} ({value}) in data/settings.toml must be one of the following:
 1: notify the mods
 2: autoban and notify the mods
 3: autoban silently"""}),
         Validator('console_log_level', 'file_log_level',
-                  is_in=["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"], messages={"operations": """{name} ({value}) in config/settings.toml must be one of the following:
+                  is_in=["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"], messages={"operations": """{name} ({value}) in data/settings.toml must be one of the following:
 CRITICAL, ERROR, WARNING, INFO, DEBUG"""}),
-        Validator('dry_run', 'exclude_mods', 'safe_mode',
-                  is_type_of=bool, messages={"operations": "{name} ({value}) in config/settings.toml must be one of: true, false"}),
+        Validator('dry_run', 'exclude_mods', 'safe_mode', 'custom_point_mod_notes',
+                  is_type_of=bool, messages={"operations": "{name} ({value}) in data/settings.toml must be one of: true, false"}),
     ]
 )
 
