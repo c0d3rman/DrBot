55253aee5b0b32ba89d94e51391d7128158c536f
Yoni Lerner <yonilern@gmail.com>
2023-09-09 14:56:20 -0700

    Improved ViolationsNotifier logic

--- drbot/handlers/ViolationsNotifierHandler.py
+++ drbot/handlers/ViolationsNotifierHandler.py
@@ -24,33 +24,33 @@ class ViolationsNotifierHandler(Handler[ModmailConversation]):
         if not re.match(fr"^u/[^ ]+ is (?:temporarily|permanently) banned from r/{settings.subreddit}$", item.subject, re.IGNORECASE):
             return
 
-        # Get username from ban message title
-        result = re.search(fr"^u/([^ ]+) is (?:temporarily|permanently) banned from r/{settings.subreddit}$", item.subject, re.IGNORECASE)
-        if result is None:
-            log.error(f"Unexpected regex error for modmail {item.id} - this shouldn't happen.")
+        # Make sure the user isn't muted, since for some reason reddit freaks out if we try to message them
+        if len(reddit().request(method="GET", path="/r/DebateReligion/about/muted",
+                                params={"user": item.participant})['data']['children']) > 0:
+            log.info(f"Couldn't send a violations notice to u/{item.participant} on modmail {item.id} since they are muted and reddit freaks out about that.")
             return
-        username = result.group(1)
 
         # Find the ban message's associated ViolationInterval by matching their timestamps.
         # Sadly there's no ID or similar that we can use, but unless you're banning a user multiple times a minute it really shouldn't be an issue.
         ban_date = datetime.fromisoformat(item.messages[0].date)
-        violations = self.points_handler.get_violations(username)[:-1]  # Don't include violations after the most recent ban
-        closest_interval = min(violations, key=lambda interval: interval.ban.created_at)
-        closest_date = datetime.fromtimestamp(closest_interval.ban.created_at, timezone.utc)
+        violations = self.points_handler.get_violations(item.participant)[:-1]  # Don't include violations after the most recent ban
+        interval_distances = [(interval, abs(datetime.fromtimestamp(interval.ban.created_at, timezone.utc) - ban_date)) for interval in violations]
+        closest_interval, time_gap = min(interval_distances, key=lambda p: p[1])
         epsilon = timedelta(hours=1)  # The maximum time gap before we consider a ban message not associated with a ban anymore
-        if abs(ban_date - closest_date) >= epsilon:  # Technically the ban message should always come after the ban, but who knows what Reddit might do
-            log.warning(f"No matching ban could be found for ban message {item.id}, so no violations notice could be sent.")
+        if time_gap >= epsilon:  # Technically the ban message should always come after the ban, but who knows what Reddit might do, so we compare the absolute time gap
+            log.warning(f"No matching ban could be found for ban message {item.id} of user u/{item.participant}, so no violations notice could be sent.")
             return
+        log.debug(f"Found a matching ban {closest_interval.ban.id} within {time_gap} of ban message {item.id}.")
 
         # Prepare modmail message
         violation_string = closest_interval.to_string(self.points_handler, include_points=False, relevant_only=True)
         if len(violation_string.strip()) == 0:
-            log.info(f"Skipping violations notice for banned user u/{username} because they have no relevant violations on record.")
+            log.info(f"Skipping violations notice for banned user u/{item.participant} because they have no relevant violations on record.")
             return
         message = f"Beep boop, I'm a robot. Here's a list of recent violations which contributed to your ban:\n\n"
         message += violation_string
 
-        log.info(f"Sending banned user u/{username} a summary of their violations for ban notice {item.id}.")
+        log.info(f"Sending banned user u/{item.participant} a summary of their violations for ban notice {item.id}.")
 
         if settings.dry_run:
             log.info(f"""[DRY RUN: would have sent the following reply to modmail {item.id}:
