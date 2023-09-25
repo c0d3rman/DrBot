93ef6b15f5afc294eb956e118c41bc2956cd2271
Yoni Lerner <yonilern@gmail.com>
2023-09-25 16:33:44 -0700

    Added AdminWatcher setting

--- src/drbot/botlings/AdminWatcher.py
+++ src/drbot/botlings/AdminWatcher.py
@@ -9,6 +9,10 @@ from ..Botling import Botling
 class AdminWatcher(Botling):
     """Scans the modlog for actions by reddit's admins."""
 
+    default_settings = {
+        "modmail": True,  # When we find an admin action, should we send a modmail about it? (Otherwise we just log)
+    }
+
     def setup(self) -> None:
         self.DR.streams.modlog.subscribe(self, self.handle)
 
@@ -25,10 +29,16 @@ class AdminWatcher(Botling):
             kind = "post"
         else:
             # Strange action, send a simple modmail and return
-            reddit.DR.send_modmail(subject=f'Admins took action "{item.action}" in your sub',
-                                 body=f"Reddit's Anti-Evil Operations took action {item.action} in your sub. See DRBOT's log for more details.")
-            log.info(f"Full info for unknown action type:\n\n{vars(item)}")
+            if self.DR.settings.modmail:
+                reddit.DR.send_modmail(subject=f'Admins took action "{item.action}" in your sub',
+                                   body=f"Reddit's Anti-Evil Operations took action {item.action} in your sub. See DRBOT's log for more details.")
+            log.info(f'Admins took unknown action "{item.action}". Full info:\n\n{vars(item)}')
             return
 
-        reddit.DR.send_modmail(subject=f"Admins removed a {kind} in your sub",
-                             body=f"On {time}, reddit's Anti-Evil Operations removed a [{kind}](https://reddit.com{item.target_permalink}) by u/{item.target_author} in your sub.")
+        message = f"On {time}, reddit's Anti-Evil Operations removed a [{kind}](https://reddit.com{item.target_permalink}) by u/{item.target_author} in your sub."
+        if self.DR.settings.modmail:
+            reddit.DR.send_modmail(subject=f"Admins removed a {kind} in your sub", body=message)
+        log.info(message)
+    
+    def validate_settings(self) -> None:
+        assert isinstance(self.DR.settings.modmail, bool), "modmail must be a bool"
