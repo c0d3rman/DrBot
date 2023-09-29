b6a33e3ae1080a253f4abc59488390759dd49835
Yoni Lerner <yonilern@gmail.com>
2023-09-29 01:58:15 -0700

    Added state to ModmailMessageStream
    And removed leftover testing limit

--- src/drbot/streams/ModmailMessageStream.py
+++ src/drbot/streams/ModmailMessageStream.py
@@ -10,12 +10,16 @@ from .TimeGuardedStream import TimeGuardedStream
 class ModmailMessageStream(TimeGuardedStream[ModmailMessage]):
     """A stream of modmail messages, sorted from oldest to newest without regard for which conversation they're in."""
 
+    def __init__(self, name: str | None = None, state: str = "all") -> None:
+        super().__init__(name=name)
+        self.state = state  # Must happen here since get_latest_item is called before setup
+
     def get_items_raw(self) -> Iterable[ModmailMessage]:
         heap = []
         heapq.heapify(heap)
 
         conversation_i = 0
-        for conversation in reddit.sub.modmail.conversations(limit=10, sort="recent"):
+        for conversation in reddit.sub.modmail.conversations(state=self.state, limit=None, sort="recent"):
             message_i = 0
             conversation_i += 1
 
@@ -35,7 +39,7 @@ class ModmailMessageStream(TimeGuardedStream[ModmailMessage]):
         return datetime.fromisoformat(item.date).astimezone(timezone.utc)
 
     def get_latest_item(self) -> ModmailMessage | None:
-        latest_conversation = next(reddit.sub.modmail.conversations(limit=1, sort="recent"), None)
+        latest_conversation = next(reddit.sub.modmail.conversations(state=self.state, imit=1, sort="recent"), None)
         if latest_conversation is None:
             return None
         return latest_conversation.messages[-1]
